db244e218081737f16d7620ce72b994c26d20302
ThePrimeagen <the.primeagen@aol.com>
2026-08-05 09:31:31 +0530

    Deployed

--- Intro-To-Web-Development/JavaScript/modal/app.js
+++ Intro-To-Web-Development/JavaScript/modal/app.js
@@ -1,3 +1,11 @@
 const openModalButton = document.getElementById("open-modal-btn");
 const closeModalButton = document.getElementById("close-modal-btn");
-const modalEl = document.getElementById('modal');
+const modalOverlay = document.getElementById("modal-overlay");
+
+openModalButton.addEventListener("click", () => {
+  modalOverlay.classList.toggle("hidden");
+});
+
+closeModalButton.addEventListener("click", () => {
+  modalOverlay.classList.toggle("hidden");
+});
