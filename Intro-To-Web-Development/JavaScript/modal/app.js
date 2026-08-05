f7e7cc66f277369c83fedd9b593100b9149b10a5
ThePrimeagen <the.primeagen@aol.com>
2026-08-05 09:07:38 +0530

    Modal

---
+++ Intro-To-Web-Development/JavaScript/modal/app.js
@@ -0,0 +1,3 @@
+const openModalButton = document.getElementById("open-modal-btn");
+const closeModalButton = document.getElementById("close-modal-btn");
+const modalEl = document.getElementById('modal');
