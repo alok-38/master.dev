1f10b12b806c487578cb1006faea53d232c85b78
ThePrimeagen <the.primeagen@aol.com>
2026-09-14 09:02:30 +0530

    flow control

---
+++ Intro-To-Web-Development/JS/flow.js
@@ -0,0 +1,7 @@
+if (2 + 2 === "4") {
+  console.log(
+    "Oh thank god, the fundamental principals of mathematics still hold true.",
+  );
+} else {
+  console.log("Uh, panic?");
+}
---
+++ Intro-To-Web-Development/JS/party.js
@@ -0,0 +1,10 @@
+let friendsAtYourParty = 0;
+let numberOfTimesToRepeat = 10;
+
+for (let i = 0; i <= numberOfTimesToRepeat; i++) {
+  let dog = "blah";
+  friendsAtYourParty++;
+}
+
+console.log(dog);
+console.log(friendsAtYourParty);
